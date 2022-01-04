e5d6bfc42e95ba757acac927d240b07a6ec8dd1a
JEPark <jepark2208@gamil.com>
2022-01-04 16:04:50 +0900

    [IMAGE OPTIMIZATION]: Image Optimization

--- supergulp/gulpfile.babel.js
+++ supergulp/gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
   pug: {
-    watch: "src/**/*/pug",
+    watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build",
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img",
   },
 };
 
@@ -19,15 +24,18 @@ const clean = () => del(["build/"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+const img = () =>
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
-// 만약 webserver와 watch를 동시에 하고 싶다면 series대신 parallel을 사용한다
-const postDev = gulp.series([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
