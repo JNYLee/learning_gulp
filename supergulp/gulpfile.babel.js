39306e253d5a197d1ae5345a8bcdca764e278794
JEPark <jepark2208@gamil.com>
2022-01-04 12:06:24 +0900

    [WEBSERVER AND WATCHING FILES]: Watching Files

--- supergulp/gulpfile.babel.js
+++ supergulp/gulpfile.babel.js
@@ -5,6 +5,7 @@ import ws from "gulp-webserver";
 
 const routes = {
   pug: {
+    watch: "src/**/*/pug",
     src: "src/*.pug",
     dest: "build",
   },
@@ -13,15 +14,20 @@ const routes = {
 const pug = () =>
   gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del([routes.pug.dest]);
+const clean = () => del(["build/"]);
 
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
 
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-const postDev = gulp.series([webserver]);
+// 만약 webserver와 watch를 동시에 하고 싶다면 series대신 parallel을 사용한다
+const postDev = gulp.series([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
